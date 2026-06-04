bbefa27c4bd67d553e0eb123efa5b5bb3e5f70ec
elhadjmamadouadamadiallo <elhadjmamadouadamadiallo@gmail.com>
2026-06-04 10:32:10 +0000

    Add service worker caching and event listeners

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,4 @@
-
+const CACHE = "makity-v1";
+self.addEventListener("install", (event) => { self.skipWaiting(); });
+self.addEventListener("activate", (event) => { event.waitUntil(self.clients.claim()); });
+self.addEventListener("fetch", (event) => { return; });
